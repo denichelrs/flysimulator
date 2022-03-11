ba0cae63d2ff43403f98f3139b166f6ce98f7396
denichelrs <100004807+denichelrs@users.noreply.github.com>
2022-03-11 18:12:03 +0100

    Update token.js

--- src/token.js
+++ src/token.js
@@ -1,3 +1,11 @@
-function token(){
-    
+function token(i){
+    const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let t = '';
+    for(let i = 0; length(i) <= 10; ++i){
+        t += Math.random(characters);
+        console.log(t);
+    }
 }
+
+consol = token(i);
+return consol;
